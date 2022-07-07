b571ceb7794659ced19a08170133a29d068a3bcc
Lintang Bongkar Girana <96236126+lintabong@users.noreply.github.com>
2022-07-07 00:55:37 -0700

    Update main.ino

--- main.ino
+++ main.ino
@@ -84,42 +84,41 @@ void setup() {
     Serial.println("SPIFFS mounted successfully");
   }
 
-  Serial.print("IP Address: http://");
-  Serial.println(WiFi.localIP());
+  Serial.print("IP Address: http://"); Serial.println(WiFi.localIP());
 
   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
 
   // OV2640 camera module
   camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
+  config.ledc_channel  = LEDC_CHANNEL_0;
+  config.ledc_timer    = LEDC_TIMER_0;
+  config.pin_d0        = Y2_GPIO_NUM;
+  config.pin_d1        = Y3_GPIO_NUM;
+  config.pin_d2        = Y4_GPIO_NUM;
+  config.pin_d3        = Y5_GPIO_NUM;
+  config.pin_d4        = Y6_GPIO_NUM;
+  config.pin_d5        = Y7_GPIO_NUM;
+  config.pin_d6        = Y8_GPIO_NUM;
+  config.pin_d7        = Y9_GPIO_NUM;
+  config.pin_xclk      = XCLK_GPIO_NUM;
+  config.pin_pclk      = PCLK_GPIO_NUM;
+  config.pin_vsync     = VSYNC_GPIO_NUM;
+  config.pin_href      = HREF_GPIO_NUM;
+  config.pin_sscb_sda  = SIOD_GPIO_NUM;
+  config.pin_sscb_scl  = SIOC_GPIO_NUM;
+  config.pin_pwdn      = PWDN_GPIO_NUM;
+  config.pin_reset     = RESET_GPIO_NUM;
+  config.xclk_freq_hz  = 20000000;
+  config.pixel_format  = PIXFORMAT_JPEG;
 
   if (psramFound()) {
-    config.frame_size = FRAMESIZE_QVGA;
+    config.frame_size   = FRAMESIZE_QVGA;
     config.jpeg_quality = 10;
-    config.fb_count = 2;
+    config.fb_count     = 2;
   } else {
-    config.frame_size = FRAMESIZE_CIF;
+    config.frame_size   = FRAMESIZE_CIF;
     config.jpeg_quality = 12;
-    config.fb_count = 1;
+    config.fb_count     = 1;
   }
   // Camera init
   esp_err_t err = esp_camera_init(&config);
@@ -174,10 +173,7 @@ void capturePhotoSaveSpiffs( void ) {
     
     File file = SPIFFS.open(FILE_PHOTO, FILE_WRITE);
 
-    if (!file) {
-      Serial.println("Failed to open file in writing mode");
-    }
-    else {
+    IF (file) {
       file.write(fb->buf, fb->len);
       Serial.print("Size: ");
       Serial.print(file.size());
