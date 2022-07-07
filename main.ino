baa94191bc822b503524827759355c083802c404
Lintang Bongkar Girana <96236126+lintabong@users.noreply.github.com>
2022-07-07 00:53:40 -0700

    Update main.ino

--- main.ino
+++ main.ino
@@ -65,23 +65,6 @@ const char index_html[] PROGMEM = R"rawliteral(
   </div>
   <div><img src="saved-photo" id="photo" width="70%"></div>
 </body>
-<script>
-  var deg = 0;
-  function capturePhoto() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', "/capture", true);
-    xhr.send();
-  }
-  function rotatePhoto() {
-    var img = document.getElementById("photo");
-    deg += 90;
-    if(isOdd(deg/90)){ document.getElementById("container").className = "vert"; }
-    else{ document.getElementById("container").className = "hori"; }
-    img.style.transform = "rotate(" + deg + "deg)";
-  }
-  
-  function isOdd(n) { return Math.abs(n % 2) == 1; }
-</script>
 </html>)rawliteral";
 
 void setup() {
